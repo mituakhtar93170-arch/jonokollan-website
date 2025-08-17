2526306b8a8f1d9dd484c6c66d046887e9eff05e
mituakhtar93170-arch <mituakhtar93170@gmail.com>
2025-08-17 23:16:14 +0530

    Add files via upload
    Signed-off-by: mituakhtar93170-arch <mituakhtar93170@gmail.com>

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mobile menu toggle
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.getElementById("menu").classList.toggle("show");
+});
+
+// Donation box function
+function makePayment() {
+  alert("ধন্যবাদ! দান করার জন্য QR কোড স্ক্যান করুন অথবা ম্যানুয়ালি পেমেন্ট করুন।");
+}
